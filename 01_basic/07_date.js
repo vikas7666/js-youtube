b92452ae8f1c2c3aa0b6a0b69e66ab35e5ad3f1d
VIKAS SHUKLA <38638875+vikas7666@users.noreply.github.com>
2024-01-18 17:48:51 +0000

    date in js completed

--- 01_basic/07_date.js
+++ 01_basic/07_date.js
@@ -25,6 +25,24 @@ let myDate  = new Date();
 // let createDate3  =  new Date("2023-01-17");  // Format yyyy-mm-dd
 // console.log(createDate3.toLocaleString());  // 1/17/2023, 12:00:00 AM
 
-let createDate4  =  new Date("01-17-2023");   // 
+// let createDate4  =  new Date("01-17-2023");   // 
 
-console.log(createDate4.toLocaleString());  //1/17/2023, 12:00:00 AM Format MM-dd-yyyy
+// console.log(createDate4.toLocaleString());  //1/17/2023, 12:00:00 AM Format MM-dd-yyyy
+
+// let timestamp  = Date.now();
+// console.log(timestamp);  // 1705598010295 this time is coming form 1970 
+
+// console.log(createDate4.getTime());  // 1673913600000 this time is came from defined date in millisecond
+
+// console.log(Date.now());  // 1705598361498
+
+// console.log(Math.floor(Date.now() / 1000));  // 1705598456
+
+
+let newDate = new Date();
+
+console.log(newDate.toLocaleDateString('default', {
+    weekday : "long" // clr + spacebar // it will highlight connected method
+}));
+
+// Thursday we get current week days
