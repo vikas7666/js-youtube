d7bd48d3a3b4aabd60373245ba2eddc597f412ab
VIKAS SHUKLA <38638875+vikas7666@users.noreply.github.com>
2024-02-25 18:30:59 +0000

    arrow and scope

--- 03_basic/03_scope.js
+++ 03_basic/03_scope.js
@@ -1,1 +1,39 @@
 {} // its called scope
+
+/* function one() {
+    const username  = 'vikas';
+
+    function two() {
+       // const nameTwo  = 'hello'
+        console.log('username is',username);
+    }
+    two()
+    console.log(nameTwo)
+}
+one() */ // username is vikas
+// here we get error bcz one cannot access inside function two
+
+
+// function expression 
+// In js variable is very power it can hold anything such as json etc
+
+// const abc  = function name() {
+//     console.log('hello')
+// }
+
+// abc() // hello
+
+//console.log('function calling before declaration',vikas('abc'));
+// function vikas(num) {
+//     console.log('num',num)
+//     return  num ;
+// }
+// num abc
+// function calling before declaration abc
+
+console.log(addTwo(5,6)); //11
+console.log(x(5,6));
+const x = function addTwo(num1,num2) {
+    return num1 + num2;
+}
+// getting error  ReferenceError: addTwo is not defined
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-https://youtu.be/eWwge2YpHhc?list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&t=72
+https://youtu.be/9ksqBa8_txM?list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&t=635
 
 codespaces
 https://github.com/codespaces
